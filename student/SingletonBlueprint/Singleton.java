0a5f67baaefc196f114b7a1556a1c97301a16fad
aboutamgh <anourboutamgharin@gmaile.com>
2025-11-21 15:08:02 +0100

    bm

---
+++ student/SingletonBlueprint/Singleton.java
@@ -0,0 +1,38 @@
+// public class Singleton {
+//     private static Singleton instance;
+
+//     private Singleton() {
+//     }
+
+//     public static Singleton getInstance() {
+//         if (instance == null){
+//             instance = new Singleton();
+//             return instance;
+//         }else{
+//             return instance;
+//         }
+        
+//     }
+
+//     public String showMessage() {
+//         return "Hello, I am a singleton!";
+//     }
+// }
+
+    public class Singleton {
+        private static Singleton instance;
+
+        private Singleton() {
+        }
+
+        public static Singleton getInstance() {
+            if (instance == null) {
+                instance = new Singleton();
+            }
+            return instance;
+        }
+
+        public String showMessage() {
+            return "Hello, I am a singleton!";
+        }
+    }
